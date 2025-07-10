e4f987d0c7d16725d1ce7726c0ae49bec12d26d3
Patypatii <patrickwambugu707@gmail.com>
2025-07-10 07:37:20 +0300

    exected the method

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿int[] a = { 1, 2, 3, 4, 5 };
+﻿// int[] a = { 1, 2, 3, 4, 5 };
 
-Console.WriteLine("Contents of Array:");
-PrintArray();
+// Console.WriteLine("Contents of Array:");
+// PrintArray();
 
-void PrintArray()
+// void PrintArray()
+// {
+//     foreach (int x in a)
+//     {
+//         Console.Write($"{x} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+//method exexcution
+Console.WriteLine("Before calling a method");
+SayHello();
+Console.WriteLine("After calling a method");
+
+void SayHello()
 {
-    foreach (int x in a)
-    {
-        Console.Write($"{x} ");
-    }
-    Console.WriteLine();
+    Console.WriteLine("Hello World!");
 }
